dc10e507c5bc609ea90906c94a1e2acf666dd0fb
crawford-kamil <108445618+crawford-kamil@users.noreply.github.com>
2023-07-02 02:50:31 -0400

    Fix win counter for player and computer

--- myscripts.js
+++ myscripts.js
@@ -20,32 +20,41 @@
         let playerWins = 0;
         let computerWins = 0;
 
+        function addPlayerWins() {
+            return playerWins + 1
+        }
+
+        function addComputerWins() {
+            return computerWins + 1
+        }
+
     function playRound() {
         let playerChoice = getPlayerChoice();
-        let playerWin = playerChoice + " beats " + computerChoice + "." + " You win this round. " + "Player: " + playerWins + " Computer: " + computerWins;
-        let playerLoss = playerChoice + " loses to " + computerChoice + "." + " You lose this round. " + "Player: " + playerWins + " Computer: " + computerWins;
+        let playerWin = playerChoice + " beats " + computerChoice + "." + " You win this round. " + "Player: " + addPlayerWins() + " Computer: " + computerWins ;
+        let playerLoss = playerChoice + " loses to " + computerChoice + "." + " You lose this round. " + "Player: " + playerWins + " Computer: " + addComputerWins() ;
         if( computerChoice === playerChoice ) {
             alert( "It's a tie. " + playerChoice + " ties with " + computerChoice )
         } else if( playerChoice === "Rock" && computerChoice === "Scissors" 
             || playerChoice === "Scissors" && computerChoice === "Paper" 
             || playerChoice === "Paper" && computerChoice === "Rock" ) {
-            alert( playerWin );
             playerWins++;
+            alert( playerWin );
         } else if( playerChoice === "Rock" && computerChoice === "Paper" 
             || playerChoice === "Scissors" && computerChoice === "Rock" 
             || playerChoice === "Paper" && computerChoice === "Scissors" ) {
-            alert( playerLoss);
             computerWins++;
+                        alert( playerLoss);
         } else {
             alert( "Invalid option. Please choose Rock, Paper, or Scissors." );
         }
     }
     
+    
         let playerScore = 0;
         let computerScore = 0;
 
     function playGame() {
-        for( let i = 1; i < 6; i++ ) {
+        for( let i = 0; i < 5; i++ ) {
             playerWins >= 3 ? 
             alert( "You've won the game!" ) :
             computerWins >= 3 ? 
